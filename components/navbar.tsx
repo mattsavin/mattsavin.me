97d224ef6ad5256029945ecacc52e07d674d933b
Matthew Savin <matt.asavin@gmail.com>
2021-12-03 16:12:37 +0000

    utilise tailwind css to create a nice navbar style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,50 @@
-import Link from "next/link";
-import styles from "./navbar.module.css";
+import Link from 'next/link';
 
-const navbar = () => {
+export const Navbar = () => {
     return (
-        <nav>
-            <div className={styles.nav}>
-                <h1 className={styles.heading}>Matthew Savin</h1>
-            </div>
-
-        </nav>
+        <>
+            <nav className='flex items-center flex-wrap bg-gray-600 p-3 '>
+                <Link href='/'>
+                    <a className='inline-flex items-center p-2 mr-4 '>
+                        <span className='text-xl text-white font-bold uppercase tracking-wide'>Matthew Savin</span>
+                    </a>
+                </Link>
+                <button className=' inline-flex p-3 hover:bg-gray-200 rounded lg:hidden text-white ml-auto hover:text-white outline-none'>
+                    <svg
+                        className='w-6 h-6'
+                        fill='none'
+                        stroke='currentColor'
+                        viewBox='0 0 24 24'
+                        xmlns='http://www.w3.org/2000/svg'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M4 6h16M4 12h16M4 18h16'
+                        />
+                    </svg>
+                </button>
+                <div className='hidden w-full lg:inline-flex lg:flex-grow lg:w-auto'>
+                    <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-500 hover:text-white '>
+                                Home
+                            </a>
+                        </Link>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-500 hover:text-white'>
+                                Portfolio
+                            </a>
+                        </Link>
+                        <Link href='/'>
+                            <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-gray-500 hover:text-white'>
+                                About me
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+            </nav>
+        </>
     );
 };
-
-export default navbar;

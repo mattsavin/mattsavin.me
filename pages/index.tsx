e9c99cc93616ddcad6cfb340f2281c868539af75
Matthew Savin <matt.asavin@gmail.com>
2021-12-03 18:44:48 +0000

    convert index.tsx to use tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,28 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
     return (
-        <div className={styles.container}>
+        <div className='flex flex-col'>
             <Head>
                 <title>Under Construction</title>
                 <meta name="description" content="mattsavin.me: Currently under construction!" />
                 <link rel="icon" href={"/favicon.ico"} />
             </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-          Under Construction!
-                </h1>
+            <main className='grid justify-items-center text-lg lg:p-32 p-6 content-center h-screen text-center font-sans'>
+                <h1 className='text-5xl font-bold'>Under Construction!</h1>
 
-                <p className={styles.description}>
+                <p className='lg:p-24 p-6'>
           This site is currently under construction!
                     <br /><br />
-          It is being built using client and server side rendering using <a href="https://nextjs.org/" className={styles.link}>Next.js</a>
+          It is being built using client and server side rendering using <a href="https://nextjs.org/" className='text-blue-500 hover:underline'>Next.js</a>
                     <br />
-          Come back later to see how it&#39;s progressed or check out the code on <a href="https://github.com/lamitron/mattsavin.me" target="_blank" rel="noreferrer">GitHub: lamitron/mattsavin.me</a>
+          Come back later to see how it&#39;s progressed or check out the code on <a href="https://github.com/lamitron/mattsavin.me" target="_blank" rel="noreferrer" className='text-blue-500 hover:underline'>GitHub: lamitron/mattsavin.me</a>
                     <br />
             Pull requests are welcome and appreciated!
                 </p>
             </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-          Powered by{' '}
-                    <span className={styles.logo}>
-                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-                    </span>
-                </a>
-            </footer>
         </div>
     );
 };

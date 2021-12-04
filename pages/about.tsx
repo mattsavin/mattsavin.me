975eb3e16aa338ae21470a018b0f4d2b5afa5d67
Matthew Savin <matt.asavin@gmail.com>
2021-12-04 22:41:52 +0000

    add an animation to navbar buttons, begin content to about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,6 @@
 import {NextPage} from "next";
 import Head from "next/head";
+import styles from "./about.module.css";
 
 const About: NextPage = () => {
     return(
@@ -8,7 +9,17 @@ const About: NextPage = () => {
                 <title>About Me: Matthew Savin</title>
                 <meta name="description" content="About Matthew Savin: an aspiring and talented young computer scientist." />
             </Head>
+            <main>
+                <section className={styles.mainHeader}>
+                    <div className={styles.headingLeft}>
+                        <h1 className={styles.headingLeft}>Hi! I&#39;m Matthew <br />An aspiring young computer scientist</h1>
+                    </div>
+                    <div className={styles.headingRight}>
 
+                    </div>
+                </section>
+
+            </main>
         </>
     );
 };

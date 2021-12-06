f2d304f46486eb8105960fa281a5c4234465754d
Matthew Savin <matt.asavin@gmail.com>
2021-12-06 11:50:59 +0000

    delete portfolio link as hiding it wasn't enough apparently

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -52,9 +52,6 @@ const Navbar: NextPage = (): JSX.Element => {
                         <Link href='/'>
                             <a className={`${styles.link} lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center justify-center`}>Home</a>
                         </Link>
-                        <Link href={'/portfolio'}>
-                            <a className={`${styles.link} ${styles.hidden} lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center justify-center`}>Portfolio</a>
-                        </Link>
                         <Link href={'/about'}>
                             <a className={`${styles.link} lg:inline-flex lg:w-auto w-full px-3 py-2 rounded items-center justify-center`}>About me</a>
                         </Link>

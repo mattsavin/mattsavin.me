d5813ee866d1038b54391f2ccdb657107c0043d0
Matthew Savin <matt.asavin@gmail.com>
2021-12-07 12:05:45 +0000

    make production footer the old version

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,26 +4,36 @@ import {NextPage} from "next";
 import styles from "./footer.module.css";
 
 const Footer: NextPage = () => {
-    return (
-        <>
-            <footer>
-                <div className={styles.footer}>
-                    <div className={styles.footerGroup}>
-                        <span className={styles.footerSection}>
-                            <h1>Get in Contact</h1>
-                        </span>
-                        <span className={styles.footerSection}>
-                            <h1>Links</h1>
+    if (process.env.NODE_ENV == "production") {
+        return (
+            <>
+                <footer className={styles.tempFooter}>
+                    <h1>mattsavin.me</h1>
+                </footer>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <footer className={styles.footerAbsolute}>
+                    <div className={styles.footer}>
+                        <div className={styles.footerGroup}>
+                            <span className={styles.footerSection}>
+                                <h1>Get in Contact</h1>
+                            </span>
+                            <span className={styles.footerSection}>
+                                <h1>Links</h1>
+                            </span>
+                        </div>
+                        <span className={styles.footerBase}>
+                            <h1>mattsavin.me</h1>
                         </span>
                     </div>
-                    <span className={styles.footerBase}>
-                        <h1>mattsavin.me</h1>
-                    </span>
-                </div>
-                <div className={styles.footerRelative}/>
-            </footer>
-        </>
-    );
+                    <div className={styles.footerRelative}/>
+                </footer>
+            </>
+        );
+    }
 };
 
 export default Footer;

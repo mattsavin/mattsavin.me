3b5dab990837a55e65a6673844033486333b9169
Matthew Savin <matt.asavin@gmail.com>
2021-12-07 12:14:44 +0000

    allow translation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,6 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <title>Matthew Savin</title>
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="Content-Language" content="en" />
-                <meta name="google" content="notranslate" />
                 <link rel="icon" href={"/favicon.ico"}/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>

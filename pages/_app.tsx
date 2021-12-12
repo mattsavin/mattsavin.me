ea41025e03fdeab6bac2342f03bddef8a873ada9
Matthew Savin <matt_savin@outlook.com>
2021-12-12 21:44:51 +0000

    add gtag to use Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,22 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Layout from "../components/layout";
 import Head from "next/head";
-import React from 'react';
+import Script from "next/script";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url: URL) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
     return(
         <>
             <Head>
@@ -17,6 +30,21 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Layout>
+                <Script
+                    strategy='lazyOnload'
+                    src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+                />
+                <Script id='ga-analytics'>
+                    {
+                        `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+            
+                        gtag('config', '${gtag.GA_TRACKING_ID}');
+                        `
+                    }
+                </Script>
                 <Component {...pageProps} />
             </Layout>
         </>

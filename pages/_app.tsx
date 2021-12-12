fdd4642d5f506f87f45b70e5473e288c6cbda07d
Matthew Savin <matt.asavin@gmail.com>
2021-12-12 22:38:39 +0000

    remove Google Analytics code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,9 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Layout from "../components/layout";
 import Head from "next/head";
-import Script from "next/script";
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const router = useRouter();
-    useEffect(() => {
-        const handleRouteChange = (url: URL) => {
-            gtag.pageview(url);
-        };
-        router.events.on('routeChangeComplete', handleRouteChange);
-        return () => {
-            router.events.off('routeChangeComplete', handleRouteChange);
-        };
-    }, [router.events]);
     return(
         <>
             <Head>
@@ -30,21 +17,6 @@ function MyApp({ Component, pageProps }: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
             <Layout>
-                <Script
-                    strategy='lazyOnload'
-                    src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-                />
-                <Script id='ga-analytics'>
-                    {
-                        `
-                        window.dataLayer = window.dataLayer || [];
-                        function gtag(){dataLayer.push(arguments);}
-                        gtag('js', new Date());
-            
-                        gtag('config', '${gtag.GA_TRACKING_ID}');
-                        `
-                    }
-                </Script>
                 <Component {...pageProps} />
             </Layout>
         </>

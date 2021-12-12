492c97845fb0ff83e4b12b3c0eedb29868d1c7cf
Matthew Savin <matt_savin@outlook.com>
2021-12-12 19:18:11 +0000

    Edit footer to include more information

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,6 +2,7 @@
 
 import {NextPage} from "next";
 import styles from "../styles/footer.module.css";
+import { SiNextdotjs } from "react-icons/si";
 
 const Footer: NextPage = () => {
     if (process.env.NODE_ENV == "production") {
@@ -17,17 +18,29 @@ const Footer: NextPage = () => {
             <>
                 <footer className={styles.footerAbsolute}>
                     <div className={styles.footer}>
-                        <div className={styles.footerGroup}>
+                        <div className={styles.columnGroup}>
                             <span className={styles.footerSection}>
-                                <h1>Get in Contact</h1>
+                                <h2>Found an issue with this page? <a href={"https://github.com/lamitron/mattsavin.me/"}>Fix it on GitHub</a></h2>
                             </span>
                             <span className={styles.footerSection}>
-                                <h1>Links</h1>
+                                <h2>Made with&nbsp;
+                                    <a href={"https://nextjs.org/"} className={styles.inlineIcon}>
+                                        <SiNextdotjs />
+                                    </a>
+                                    &nbsp;by Matthew Savin</h2>
+                            </span>
+                            <div className={styles.rowGroup}>
+                                <span className={`${styles.rowSection} ${styles.footerSection}`}>
+                                    <h1>Get in Contact</h1>
+                                </span>
+                                <span className={`${styles.rowSection} ${styles.footerSection}`}>
+                                    <h1>Links</h1>
+                                </span>
+                            </div>
+                            <span className={styles.footerBase}>
+                                <h1>mattsavin.me, All content &copy; 2021 Matthew Savin</h1>
                             </span>
                         </div>
-                        <span className={styles.footerBase}>
-                            <h1>mattsavin.me</h1>
-                        </span>
                     </div>
                     <div className={styles.footerRelative}/>
                 </footer>

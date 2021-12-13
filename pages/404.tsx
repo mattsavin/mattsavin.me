d88bd29977d250179907ec77b1b84d7a1bd01812
Matthew Savin <matt.asavin@gmail.com>
2021-12-13 00:34:13 +0000

    restyle 404.tsx

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,8 @@ const Custom404 = () => {
     return (
         <main className={styles.fullheight}>
             <div className={styles.center}>
-                <h1 className={styles.mainHeader}>404 Not Found</h1>
+                <h1 className={styles.mainHeader}>404</h1>
+                <h2 className={styles.subheading}>This page could not found</h2>
                 <p><Link href={"/"}><a>Return to the homepage</a></Link></p>
             </div>
         </main>

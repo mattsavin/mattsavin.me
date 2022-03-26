3725bee8fb19bc876de0561aa70ae55a95217fd4
Matthew Savin <matt.asavin@gmail.com>
2022-03-26 14:55:21 +0000

    move stats to separate react components

---
+++ components/indexStats/statHolder.tsx
@@ -0,0 +1,13 @@
+import styles from "../../pages/index.module.css";
+
+const Stat = (props: any) => {
+    return (
+        <span className={styles.stat}>
+            <h2>{props.amount}</h2>
+            <div className={styles.statDivider} />
+            <p>{props.achievement}</p>
+        </span>
+    );
+};
+
+export default Stat;
